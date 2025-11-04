63d413d4106361fec98d40c9e3770d6d995d83ab
taku-hira <goldenslumbers0902@gmail.com>
2025-11-04 22:50:49 +0900

    変数・定数

---
+++ variable.swift
@@ -0,0 +1,12 @@
+// 変数と定数
+
+// 変数：　var 変数名: 型 = 初期値
+var message: String = "Hello, Swift!"
+print(message) // 出力: Hello, Swift!
+
+// 定数：　let 定数名: 型 = 初期値
+let number: Int = 42
+print(number) // 出力: 42
+//number = 50 // エラー: 定数は再代入できません
+
+

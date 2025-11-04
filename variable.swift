bced318bd4e0fb345b8b771dd6353536c2b3a50d
taku-hira <goldenslumbers0902@gmail.com>
2025-11-04 23:14:55 +0900

    定数・変数

--- variable.swift
+++ variable.swift
@@ -9,4 +9,29 @@ let number: Int = 42
 print(number) // 出力: 42
 //number = 50 // エラー: 定数は再代入できません
 
+// 型の確認方法
+print(type(of: message)) // 出力: String
+print(type(of: number)) // 出力: Int
 
+
+// スコープ
+
+// ローカルスコープ
+func someFunction() {
+    let a = "a"
+    print(a) // 出力: a
+}
+
+// print(a) // エラー: 'a' はスコープ外です
+someFunction()
+
+// グローバルスコープ
+
+let globalA = "a"
+
+func someFunction2() {
+    print(globalA) // 出力: a
+}
+
+print(globalA) // 出力: a
+someFunction2()
